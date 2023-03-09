4ec7b240d63284a769282708986e39b66ada5625
Gabriel Alves <37886654+GabrielDragone@users.noreply.github.com>
2023-03-09 19:21:16 -0300

    first commit

---
+++ src/test/kotlin/com/gabrieldragone/springmongodb/SpringMongodbApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.gabrieldragone.springmongodb
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringMongodbApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
